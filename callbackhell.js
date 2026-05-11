d3fd885c337e5bc8638e010c4b4cbae9b96a89e4
chanuka randitha <chanukaranditha99@gmail.com>
2026-05-11 23:28:56 +0530

    Add callbackhell.js with nested callback functions for data retrieval

---
+++ callbackhell.js
@@ -0,0 +1,28 @@
+console.log("start");
+
+function getData(id, callback) {
+  setTimeout(() => {
+    callback({ items: [1, 2, 3, 4, 5] });
+  }, 3000);
+}
+
+function getPrice(item, callback) {
+  setTimeout(() => {
+    callback({ price: 500 });
+  }, 5000);
+}
+
+const items = getData(1, (items) => {
+  console.log(items);
+  getPrice(items[0], (price) => {
+    getPrice(items[0], (price) => {
+      getPrice(items[0], (price) => {
+        getPrice(items[0], (price) => {
+          console.log(price);
+        });
+      });
+    });
+  });
+});
+
+console.log("end");
