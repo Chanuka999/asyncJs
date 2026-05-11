5a5ee670c0898f2c92e79e37430b450054fddef6
chanuka randitha <chanukaranditha99@gmail.com>
2026-05-11 23:29:10 +0530

    Add issue.js with asynchronous data retrieval example

---
+++ issue.js
@@ -0,0 +1,13 @@
+console.log("start");
+
+function getData(id, callback) {
+  setTimeout(() => {
+    callback({ items: [1, 2, 3, 4, 5] });
+  }, 3000);
+}
+
+const items = getData(1, (items) => {
+  console.log(items);
+});
+
+console.log("end");
